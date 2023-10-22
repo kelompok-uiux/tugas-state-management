f831ab50f063363154d13075ccdb8c1b6cc92c05
DvaDvaD <davidlou0810@gmail.com>
2023-10-22 21:38:20 +0700

    mobx

---
+++ main.js
@@ -0,0 +1,44 @@
+import { makeObservable, observable, action } from "mobx";
+
+document.addEventListener("DOMContentLoaded", () => {
+  class ColorStore {
+    greenBox = "green";
+    blueBox = "blue";
+
+    constructor() {
+      makeObservable(this, {
+        greenBox: observable,
+        blueBox: observable,
+        toggleColor: action,
+      });
+    }
+
+    toggleColor(targetColor) {
+      if (targetColor === "blue") {
+        if (this.blueBox === "red") {
+          this.blueBox = "blue";
+        }
+      } else {
+        this.blueBox = "red";
+      }
+    }
+  }
+
+  const colorStore = new ColorStore();
+
+  const changeColor = (targetColor) => {
+    console.log(targetColor, colorStore.blueBox, colorStore.greenBox);
+    colorStore.toggleColor(targetColor);
+    updateBoxStyles();
+  };
+
+  window.changeColor = changeColor;
+
+  const updateBoxStyles = () => {
+    const greenBox = document.getElementById("greenBox");
+    const blueBox = document.getElementById("blueBox");
+
+    greenBox.className = `color-box ${colorStore.greenBox}`;
+    blueBox.className = `color-box ${colorStore.blueBox}`;
+  };
+});
